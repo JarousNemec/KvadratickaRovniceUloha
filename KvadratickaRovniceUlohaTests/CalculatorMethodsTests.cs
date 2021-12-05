635b12b2151e72346bd6590e04d1cfde28d2aeaa
JarousNemec <jarous.nemec@gmail.com>
2021-12-05 16:04:29 +0100

    Added unit test for calculator methods

---
+++ KvadratickaRovniceUlohaTests/CalculatorMethodsTests.cs
@@ -0,0 +1,37 @@
+﻿using KvadratickaRovniceUloha;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KvadratickaRovniceUloha.Tests
+{
+    [TestClass()]
+    public class CalculatorMethodsTests
+    {
+        [TestMethod()]
+        public void SolveEquationWhenDiscriminantIsBiggerThanZeroTest()
+        {
+            double a = 2;
+            double b = 9;
+            double D = 49;
+            double[] solution = CalculatorMethods.SolveEquationWhenDiscriminantIsBiggerThanZero(D, b, a);
+            Assert.AreEqual(solution[0], -0.5);
+            Assert.AreEqual(solution[1], -4);
+        }
+        [TestMethod()]
+        public void CalculateDiscriminant()
+        {
+            double D = CalculatorMethods.CalculateDiscriminant(2, 4, 2);
+            Assert.AreEqual(D, 0);
+        }
+        [TestMethod()]
+        public void SolveEquationWhenDiscriminantIsZero()
+        {
+            double result = CalculatorMethods.SolveEquationWhenDiscriminantIsZero(2, 4);
+            Assert.AreEqual(result, -1);
+        }
+    }
+}
